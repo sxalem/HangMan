990fce9f40e84021d55319e6c205081da77cc036
sxalem <167420289+sxalem@users.noreply.github.com>
2024-05-06 19:33:16 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from HangMan import Hangman, HumanPlayer, ComputerPlayer
-
-human_player = HumanPlayer("Salim")
-computer_player = ComputerPlayer("Alain")
-game = Hangman([human_player, computer_player])
-game.play()
